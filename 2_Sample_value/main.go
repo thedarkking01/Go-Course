74003e7d7581785116ed81017d43011bcd4a2421
thedarkking01 <sourabhsinghbais52@gmail.com>
2025-03-08 18:34:05 +0530

    data type

---
+++ 2_Sample_value/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	// int
+	fmt.Println(1)
+	// string
+	fmt.Println("hello goodbye")
+	// float 
+	fmt.Println(10.5)
+	fmt.Println(10.0/7.0)
+	// boolean
+	fmt.Println(true && false)
+    fmt.Println(true || false)
+    fmt.Println(!true)
+}
