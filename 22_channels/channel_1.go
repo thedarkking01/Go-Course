cb3950377c6d6b8bd30c0f391d744603d26c2ef1
thedakking01 <sourabhsinghbais52@gmail.com>
2025-03-27 22:56:59 +0530

    channel

---
+++ 22_channels/channel_1.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+    // Create a new channel of integers
+    ch := make(chan int)
+
+    // Start a new goroutine
+    go func() {
+        // Send a value into the channel
+        ch <- 42
+        fmt.Println("Value sent to channel.")
+    }()
+
+    // Receive the value from the channel
+    receivedValue := <-ch
+    fmt.Println("Received from channel:", receivedValue)
+}
